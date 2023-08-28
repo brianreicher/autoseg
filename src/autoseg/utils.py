858650506408aa7c1431d78c4442d55398f4eba9
brianreicher <reicher.b@northeastern.edu>
2023-08-28 14:59:45 -0400

    Refactor MSE

--- src/autoseg/utils.py
+++ src/autoseg/utils.py
@@ -1,11 +1,13 @@
 import torch
 
 
-class WeightedMSELoss(torch.nn.MSELoss):
-    def __init__(self) -> None:
-        super(WeightedMSELoss, self).__init__()
+class WeightedMTLSD_MSELoss(torch.nn.MSELoss):
+    def __init__(self, aff_lambda=1.0) -> None:
+        super(WeightedMTLSD_MSELoss, self).__init__()
 
-    def forward(self, prediction, target, weights):
+        self.aff_lambda = aff_lambda
+
+    def _calc_loss(self, prediction, target, weights):
         scaled = weights * (prediction - target) ** 2
 
         if len(torch.nonzero(scaled)) != 0:
@@ -16,3 +18,17 @@ class WeightedMSELoss(torch.nn.MSELoss):
             loss = torch.mean(scaled)
 
         return loss
+
+    def forward(
+        self,
+        pred_lsds=None,
+        gt_lsds=None,
+        lsds_weights=None,
+        pred_affs=None,
+        gt_affs=None,
+        affs_weights=None,
+    ):
+        lsd_loss = self._calc_loss(pred_lsds, gt_lsds, lsds_weights)
+        aff_loss = self.aff_lambda * self._calc_loss(pred_affs, gt_affs, affs_weights)
+
+        return lsd_loss + aff_loss
